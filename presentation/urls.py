c7995fca4065a3c2a85d7b7fa49d24dd4989a744
sonjaschnik <katharina.meder@student.uni-halle.de>
2026-07-01 01:54:50 +0200

    Update urls.py - add a new url for the readme page

--- presentation/urls.py
+++ presentation/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("do_action/", views.do_action, name="do_action"),  # view to call
+    path('', views.home, name='home'),
+    path('do_action/', views.do_action, name='do_action'),
+    path('readme/', views.readme, name='readme'),
 ]
