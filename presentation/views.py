3827455dd5dc07145ea3151a32f58a3984ab0a64
sonjaschnik <katharina.meder@student.uni-halle.de>
2026-06-30 11:03:56 +0200

    set up urls.py and views.py for the button

---
+++ presentation/views.py
@@ -0,0 +1,14 @@
+# myapp/views.py
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+def do_action(request):
+    if request.method == "POST":
+        # my server-side logic here
+        print("Button clicked! Performing action...")
+        return HttpResponse("Action completed successfully!")
+    else:
+        return redirect('home')  # prevent GET access
