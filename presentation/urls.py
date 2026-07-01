c71270d61f426023066cc17f8538fcf9b40d323c
sonjaschnik <katharina.meder@student.uni-halle.de>
2026-07-01 02:16:45 +0200

    Update urls.py

--- presentation/urls.py
+++ presentation/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('do_action/', views.do_action, name='stocks'),
+    path('do_action/', views.do_action, name='do_action'),
     path('readme/', views.readme, name='readme'),
 ]
